fdc394b865029c3dda9b248840bdf50eca0fab55
nitish-witty <nitish.anand@wittybrains.com>
2022-11-11 20:02:26 +0530

    initial commit

--- middleware/utils.js
+++ middleware/utils.js
@@ -1,18 +1,17 @@
 const jwt = require("jsonwebtoken");
 
-
 const createToken = () => {
-    return jwt.sign(
-        {
-          name: "nitish"
-        },
-        "mynameisnitishanand",
-        { expiresIn: "1h" }
-      );
-}
+  return jwt.sign(
+    {
+      name: "nitish"
+    },
+    "mynameisnitishanand",
+    { expiresIn: "1h" }
+  );
+};
 
-const verifyToken = (token)=>{
-    return jwt.verify(token, "mynameisnitishanand");
-}
+const verifyToken = (token) => {
+  return jwt.verify(token, "mynameisnitishanand");
+};
 
-module.exports = {createToken, verifyToken}
+module.exports = { createToken, verifyToken };
